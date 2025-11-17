5d6be69317eb3af398a58cce3ce19cc9625dac5f
Yash Lahase <yashlahase1234@gmail.com>
2025-11-17 13:04:17 +0530

    Cleaned repo: removed node_modules, expo, dist and added .gitignore

--- node_modules/react-native-qrcode-svg/src/utils.js
+++
@@ -1,7 +0,0 @@
-export function isString(variable) {
-    return typeof variable === "string";
-}
-
-export function isUrlString(variable) {
-    return isString(variable) && variable.startsWith("http");
-}
